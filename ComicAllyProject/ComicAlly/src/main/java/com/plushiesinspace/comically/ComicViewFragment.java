801cc77258c3b1bd23ffad987317696e098e432d
Clement Lee <clem@princeton.edu>
2013-10-16 03:06:22 -0400

    Temporarily removed fragments for easier layout

---
+++ ComicAllyProject/ComicAlly/src/main/java/com/plushiesinspace/comically/ComicViewFragment.java
@@ -0,0 +1,9 @@
+package com.plushiesinspace.comically;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by cleme_000 on 10/16/13.
+ */
+public class ComicViewFragment extends Fragment {
+}
