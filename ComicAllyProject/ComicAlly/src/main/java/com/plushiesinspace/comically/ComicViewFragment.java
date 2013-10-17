588bdd08f1dd8b96609d8bd78c8b7b887cb568d4
Clement Lee <clem@princeton.edu>
2013-10-17 03:20:39 -0400

    Began basic work on ComicViewFragment

--- ComicAllyProject/ComicAlly/src/main/java/com/plushiesinspace/comically/ComicViewFragment.java
+++ ComicAllyProject/ComicAlly/src/main/java/com/plushiesinspace/comically/ComicViewFragment.java
@@ -1,9 +1,21 @@
 package com.plushiesinspace.comically;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
+ * The fragment that contains the actual comic viewer, with images and the navigation tools.
  * Created by cleme_000 on 10/16/13.
  */
 public class ComicViewFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //return inflater.inflate(R.layout.comicview_fragment, container, false);
+        return null;
+    }
 }
