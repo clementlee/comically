8d2c5ef36df2274251586877e428560a152da64b
Clement Lee <clem@princeton.edu>
2013-10-18 00:02:01 -0400

    Renamed file for clarity

--- ComicAllyProject/ComicAlly/src/main/java/com/plushiesinspace/comically/DatabaseHelper.java
+++ ComicAllyProject/ComicAlly/src/main/java/com/plushiesinspace/comically/DatabaseHelper.java
@@ -1,12 +1,17 @@
 package com.plushiesinspace.comically;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
  * Created by cleme_000 on 10/17/13.
  */
-public class DatabaseHandler extends SQLiteOpenHelper {
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    DatabaseHelper(Context c) {
+        super(c, "lol", null, 1);
+    }
     @Override
     public void onCreate(SQLiteDatabase db) {
 
