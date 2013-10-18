a90165255b32ac9a568e55608e245de45128a0b0
Clement Lee <clem@princeton.edu>
2013-10-17 22:29:49 -0400

    Added infrastructure files

---
+++ ComicAllyProject/ComicAlly/src/main/java/com/plushiesinspace/comically/DatabaseHandler.java
@@ -0,0 +1,19 @@
+package com.plushiesinspace.comically;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by cleme_000 on 10/17/13.
+ */
+public class DatabaseHandler extends SQLiteOpenHelper {
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ ComicAllyProject/ComicAlly/src/main/java/com/plushiesinspace/comically/ImageDownloader.java
@@ -0,0 +1,8 @@
+package com.plushiesinspace.comically;
+
+/**
+ * Downloads and stores images to external storage.
+ * Created by cleme_000 on 10/17/13.
+ */
+public class ImageDownloader {
+}
